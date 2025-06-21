20d212162a8bf3a510b261891ec936d6b13792e0
ideal <ide_al@163.com>
2025-06-21 12:18:08 +0800

    feat: 删除默认样式，添加reset(清除默认 )样式

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import 'virtual:uno.css'
+import '@unocss/reset/tailwind.css'
 import router from './router'
 
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+  
