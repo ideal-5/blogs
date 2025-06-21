fd0c92fb6103a1e93bdec0bde5a7f7f75b510c02
ideal <ide_al@163.com>
2025-06-21 18:09:49 +0800

    feat: 主题切换初步基本实现<过度动画还没太好>，安装vueUse

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,12 +2,19 @@
     import gsap from 'gsap'
     import { SplitText } from 'gsap/SplitText'
     import { onMounted } from 'vue'
+    import { useDark, useToggle } from '@vueuse/core'
 
+    const isDark = useDark({
+        selector: 'html', // 作用在根元素
+        attribute: 'class',
+        valueDark: 'dark',
+        valueLight: 'light',
+    })
+    const toggleDark = useToggle(isDark)
     onMounted(() => {
         gsap.registerPlugin(SplitText)
 
         const segmenter = new Intl.Segmenter('zh', { granularity: 'word' })
-        console.log('SplitText', SplitText)
         document.fonts.ready.then(() => {
             gsap.set('.split', { opacity: 1 })
             SplitText.create('.split', {
@@ -31,13 +38,50 @@
             })
         })
     })
+
+    const changeDark = (event: MouseEvent) => {
+        const transition = document.startViewTransition(() => {
+            toggleDark()
+        })
+        transition.ready.then(() => {
+            const { clientX, clientY } = event
+
+            const radius = Math.hypot(
+                Math.max(clientX, innerWidth - clientX),
+                Math.max(clientY, innerHeight - clientY)
+            )
+
+            const clipPath = [
+                `circle(0px at ${clientX}px ${clientY}px)`,
+                `circle(${radius}px at ${clientX}px ${clientY}px)`,
+            ]
+
+            const isDark = document.documentElement.classList.contains('dark')
+
+            document.documentElement.animate(
+                {
+                    clipPath: isDark ? clipPath.reverse() : clipPath,
+                },
+                {
+                    duration: 800,
+                    easing: 'ease-in-out',
+                    pseudoElement: isDark
+                        ? '::view-transition-old(root)'
+                        : '::view-transition-new(root)',
+                }
+            )
+        })
+    }
 </script>
 
 <template>
-    <div class="size-full h-100vh text-#FFFCE1 bg-#0E100F f-c-c">
+    <div class="size-full h-100vh text-#FFFCE1 bg-#0E100F lt-sm:bg-red f-c-c dark:bg-red">
         <div class="split text-10">
             大漠孤洲起星芒，天外孤魂牵断肠 自身本是轮回客，不知何处话桑麻
         </div>
+        <button @click="changeDark" class="p-2 rounded-lg bg-gray-200">
+            {{ isDark ? '🌙' : '☀️' }}
+        </button>
 
         <a href="https://beian.miit.gov.cn" target="_blank" class="fixed bottom-1"
             >陕ICP备2025071212号</a
@@ -45,6 +89,26 @@
     </div>
 </template>
 
-<style scoped lang="scss">
-  
+<style scoped>
+    ::view-transition-old(root),
+    ::view-transition-new(root) {
+        animation: none;
+        mix-blend-mode: normal;
+    }
+
+    ::view-transition-old(root) {
+        z-index: 1;
+    }
+
+    ::view-transition-new(root) {
+        z-index: 2147483646;
+    }
+
+    html.dark::view-transition-old(root) {
+        z-index: 2147483646;
+    }
+
+    html.dark::view-transition-new(root) {
+        z-index: 1;
+    }
 </style>
